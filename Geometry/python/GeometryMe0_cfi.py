9861d6296c58e744d00603abc5afd88b96c1a276
Raul Rabadan <raul.iraq.rabadan.trejo@cern.ch>
2023-05-02 00:10:12 +0200

    test me0 stack unpacker. added me0stack mapping

---
+++ Geometry/python/GeometryMe0_cfi.py
@@ -0,0 +1,22 @@
+import FWCore.ParameterSet.Config as cms
+
+XMLIdealGeometryESSource = cms.ESSource("XMLIdealGeometryESSource",
+    geomXMLFiles = cms.vstring(
+        'gemsw/Geometry/data/materials.xml',
+        'gemsw/Geometry/data/cms.xml',
+        'gemsw/Geometry/data/cmsMother.xml',
+        'gemsw/Geometry/data/muonBase.xml',
+        'gemsw/Geometry/data/cmsMuon.xml',
+        'gemsw/Geometry/data/me0/mf.xml',
+        'gemsw/Geometry/data/gemf.xml',
+        'gemsw/Geometry/data/me0/ge0.xml',
+        'gemsw/Geometry/data/me0/muonNumbering.xml',
+        'gemsw/Geometry/data/muonSens.xml',
+        ## custom stand
+        'gemsw/Geometry/data/GEMSpecs.xml',
+        'gemsw/Geometry/data/GEMSpecsFilter.xml',
+        'gemsw/Geometry/data/me0/mf.xml',
+        'gemsw/Geometry/data/me0/tbGE0.xml',
+    ),
+    rootNodeName = cms.string('cms:OCMS')
+)
